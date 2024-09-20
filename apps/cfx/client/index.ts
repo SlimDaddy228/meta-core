1ba755e2e5e0f94fb660ab89717b961e0db4f1d8
slim <vlad.dryg@mail.ru>
2024-09-20 16:41:20 +0200

    feat: Add client application start

--- apps/cfx/client/index.ts
+++ apps/cfx/client/index.ts
@@ -1,3 +1,15 @@
 import "reflect-metadata"
+import {Application} from "@core/application";
+import {ProviderClientLoader} from "@core/loaders/provider.client.loader";
+import {NuiModule} from "@client/modules/nui/nui.module";
 
-console.log("client init2")
+function startApplication() {
+    const application = Application.create(
+        ProviderClientLoader,
+        NuiModule
+    )
+
+    application.start()
+}
+
+startApplication()
---
+++ apps/cfx/client/modules/nui/nui.menu.provider.ts
@@ -0,0 +1,5 @@
+import {Provider} from "@core/decorations/provider";
+
+@Provider()
+export class NuiMenuProvider {
+}

bc018fcdf5d36849a8e32577a7f21b5dc53da468
Slim <vlad.dryg@mail.ru>
2024-09-26 17:59:08 +0200

    fix: Remove all component ui

--- apps/web/src/library/slices/menu/index.tsx
+++ apps/web/src/library/slices/menu/index.tsx
@@ -1,9 +1,7 @@
-import {Button} from '@ui/button'
-
 export const Menu = () => {
   return (
-    <div className="w-[50%] h-[50%] flex justify-center">
-      <Button>Can i hear me</Button>
+    <div>
+      <span>2</span>
     </div>
   )
 }
